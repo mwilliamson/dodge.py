67f1eb2fbdad99d5b5777df786a7179aceeada8d
Michael Williamson <mike@zwobble.org>
2014-08-29 17:10:24 +0100

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -23,4 +23,17 @@ setup(
     keywords="data object serialise serialisation json",
     packages=['dodge'],
     install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
